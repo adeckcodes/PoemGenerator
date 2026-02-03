fde9bc0d367ee5f76175354aebe01c3b5a9c03af
adeckcodes <adeck2012@gmail.com>
2026-02-02 20:08:38 -0600

    improved the UX

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating"> Generating a poem about ${instructionsInput.value}</div>`;
   console.log("generating poem");
   console.log(`prompt: ${prompt}`);
   console.log(`context: ${context}`);
